ce33b56341c92b2c696e05089a949c590896ea8d
Isaac Martin <IsaacMartin151@gmail.com>
2026-07-01 20:29:34 -0700

    Updating index.html, bumping to 0.0.5 version of sdk. Will try to publish this build to maven

--- sdk/src/main/java/com/example/parkinglot/sdk/model/GenerationRequests.java
+++ sdk/src/main/java/com/example/parkinglot/sdk/model/GenerationRequests.java
@@ -1,10 +1,6 @@
 package com.example.parkinglot.sdk.model;
 
 public enum GenerationRequests {
-    CAR_CREATE,
-    CAR_UPDATE,
-    CAR_DELETE,
-    PARKING_LOT_CREATE,
-    PARKING_LOT_UPDATE,
-    PARKING_LOT_DELETE
+    PARKING_SPACE_UPDATE,
+    PARKING_SPACE_DELETE,
 }
