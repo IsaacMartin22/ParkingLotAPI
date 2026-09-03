3479433b126872b38b4441efc2e5f5b1c42d673a
Isaac Martin <IsaacMartin151@gmail.com>
2026-09-02 22:44:21 -0700

    Adding api to path for contact controller

--- api-service/src/main/java/apiservice/controller/ContactController.java
+++ api-service/src/main/java/apiservice/controller/ContactController.java
@@ -6,9 +6,11 @@ import jakarta.validation.constraints.NotBlank;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class ContactController {
     private final ContactService contactService;
 
